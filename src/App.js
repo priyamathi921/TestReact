33bec367d575eec1b741ddc72d82b827d41287d5
priyamathi921 <72906581+priyamathi921@users.noreply.github.com>
2022-09-13 15:33:15 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/Button'
 function App() {
   return (
     <div className="App">
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button name="Sample"></Button>
       </header>
     </div>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+
+
+function Button(props) {
+  return (
+      <button>{props.name}</button>
+  );
+}
+
+export default Button;
